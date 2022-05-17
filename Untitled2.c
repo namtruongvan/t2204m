65200b55125a81f1f201e0babc79b28e41f9780e
truongvannam <104610706+truongvannam@users.noreply.github.com>
2022-05-17 08:47:56 +0700

    bài làm

---
+++ Untitled2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>//input thu vien nhap va xuat cua C 
+int main(){
+	  short a ;
+	  long b ;
+	  long double c ;
+	  float x;
+	  char ch ;
+	  int i;
+	  
+	  //hham in ra man hinh : prontf ("")
+	  printf( "\nkich thuong cua short = %d",sizeof(a)); 
+	  printf("\nkich thuoc cua long =%d",sizeof(b)); 
+	  printf("\nkich thuong cua float = %d",sizeof (x)); 
+	  prontf("\nkich thuoc cua char= %d",sizeof (ch));
+	  printf("\nkich thuoc cua int = %d",sizeof(i));
+
+      return 0 ;  
+       
+	   }
+      
